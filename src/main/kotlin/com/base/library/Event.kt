37025f7aee02ea78e063e8d76c22564ac406aca4
Jihoi-Kang <jihoi.kang@gmail.com>
2021-06-02 09:50:41 +0900

    init

---
+++ src/main/kotlin/com/base/library/Event.kt
@@ -0,0 +1,42 @@
+package com.base.library
+
+open class Event<out T>(private val content: T) {
+
+    var hasBeenHandled = false
+        private set // Allow external read but not write
+
+    /**
+     * Returns the content and prevents its use again.
+     */
+    fun getContentIfNotHandled(): T? {
+        return if (hasBeenHandled) {
+            null
+        } else {
+            hasBeenHandled = true
+            content
+        }
+    }
+
+    fun get(action: (T) -> Unit) {
+        getContentIfNotHandled()?.let(action)
+    }
+
+    /**
+     * Returns the content, even if it's already been handled.
+     */
+    fun peekContent(): T = content
+}
+
+@MainThread
+inline fun <T> LiveData<Event<T>>.eventObserve(
+    owner: LifecycleOwner,
+    crossinline onChanged: (T) -> Unit
+): Observer<Event<T>> {
+    val wrappedObserver = Observer<Event<T>> { event ->
+        event.getContentIfNotHandled()?.let {
+            onChanged.invoke(it)
+        }
+    }
+    observe(owner, wrappedObserver)
+    return wrappedObserver
+}
---
+++ src/main/kotlin/com/base/library/ui/BaseActivity.kt
@@ -0,0 +1,10 @@
+package com.base.library
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BaseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
